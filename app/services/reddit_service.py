a028fe7e36af002344dda0ce07200defc54d672c
Tyler Etheridge <tylerjetheridge98@gmail.com>
2020-06-23 14:34:42 -0500

    update reddit api

--- app/services/reddit_service.py
+++ app/services/reddit_service.py
@@ -17,7 +17,19 @@ reddit = praw.Reddit(client_id=REDDIT_ID,
                      user_agent=REDDIT_USER_AGENT,
                      username=REDDIT_USERNAME)
 
+
 # Set to read only mode
 reddit.read_only = True
 # If logged in, should return reddit username
 print(reddit.user.me())
+print("---")
+
+# Test print ten hot posts on all
+for submission in reddit.subreddit("All").hot(limit=10):
+    print(submission.title)
+    print("---")
+
+# {
+#     post: "test text"
+#     predictions: ["subreddit1", "subreddit2", "subreddit3"]
+# }
