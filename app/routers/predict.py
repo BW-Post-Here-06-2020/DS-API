0902bff0013ad16d3a395d512b4dc890f1303bfc
kevinliu121 <kevingliu2018@gmail.com>
2020-06-25 10:31:57 -0700

    Fixed Predict.py

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -33,8 +33,8 @@ async def predict(item: Item):
     # log.info(X_new)
     # y_pred = random.choice([True, False])
     # y_pred_proba = random.random() / 2 + 0.5
-    return {
-        'title': "Is Fusion nullified for the Extreme Z Awakening Event?",
+
+    example = {'title': "Is Fusion nullified for the Extreme Z Awakening Event?",
         'post': "On JP I missed out on my chance to do SSJ3 Goku the first time so I'm doing it now. Been lucked out of rotations for most of these stages and I've noticed that for my Fusions team, LR Gogeta would NEVER fuse. I'm genuinely curious if the mechanic is nullified for the event or i'm just getting AWFUL RNG.",
-        'prediction': ["DBZDokkanBattle", "Subreddit2", "Subreddit3", "Subreddit4", "Subreddit5", "Subreddit6", "Subreddit7", "Subreddit8", "Subreddit9", "Subreddit10"]   
-    }
+        'prediction': ["DBZDokkanBattle", "Subreddit2", "Subreddit3", "Subreddit4", "Subreddit5", "Subreddit6", "Subreddit7", "Subreddit8", "Subreddit9", "Subreddit10"]}
+    return example
